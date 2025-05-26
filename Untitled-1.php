015d272c1649eccd9df9ae5f9c5cd613f4f45af9
MRafly <mraflyyydwiii@gmail.com>
2025-05-26 14:09:00 +0700

    Delete Untitled-1.php

--- Untitled-1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$target_dir = "uploads/";
-if (!is_dir($target_dir)) mkdir($target_dir, 0777, true);
-$bukti = time() . '_' . basename($_FILES["bukti"]["name"]);
-move_uploaded_file($_FILES["bukti"]["tmp_name"], $bukti);
